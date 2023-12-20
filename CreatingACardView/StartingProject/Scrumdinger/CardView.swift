3509778a20ed29fc433411a8ad41ffde1ed7dcba
TJMoore <mooreth4791@gmail.com>
2023-12-20 16:14:36 -0500

    completed cardview

--- CreatingACardView/StartingProject/Scrumdinger/CardView.swift
+++ CreatingACardView/StartingProject/Scrumdinger/CardView.swift
@@ -16,9 +16,14 @@ struct CardView: View {
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage:"clock")
+                    .padding(.trailing, 20)
             }
+            .font(.caption)
         }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
