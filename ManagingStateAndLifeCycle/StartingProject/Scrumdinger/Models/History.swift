9c6590c04bcf7432a64ff03ca6dfcbfa45707d24
TJMoore <mooreth4791@gmail.com>
2024-01-04 18:12:20 -0500

    added history log to app

---
+++ ManagingStateAndLifeCycle/StartingProject/Scrumdinger/Models/History.swift
@@ -0,0 +1,20 @@
+//
+//  History.swift
+//  Scrumdinger
+//
+//  Created by T.J. Moore on 1/4/24.
+//
+
+import Foundation
+
+struct History: Identifiable {
+    let id: UUID
+    let date: Date
+    var attendees: [DailyScrum.Attendee]
+
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
+}
--- ManagingStateAndLifeCycle/StartingProject/Scrumdinger/Views/DetailView.swift
+++ ManagingStateAndLifeCycle/StartingProject/Scrumdinger/Views/DetailView.swift
@@ -40,6 +40,17 @@ struct DetailView: View {
                     Label(attendee.name, systemImage: "person")
                 }
             }
+            Section(header: Text("History")) {
+                if scrum.history.isEmpty {
+                    Label("No Meetings Yet", systemImage: "calendar.badge.exclamationmark")
+                }
+                ForEach(scrum.history) { history in
+                    HStack {
+                        Image(systemName: "calendar")
+                        Text(history.date, style: .date)
+                    }
+                }
+            }
         }
         .navigationTitle(scrum.title)
         .toolbar {
