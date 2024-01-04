ab00666dab6d1cebddd8af2d670ba1e4820951c7
TJMoore <mooreth4791@gmail.com>
2024-01-04 17:17:51 -0500

    added sound to next speaker

--- ManagingStateAndLifeCycle/StartingProject/Scrumdinger/Views/MeetingView.swift
+++ ManagingStateAndLifeCycle/StartingProject/Scrumdinger/Views/MeetingView.swift
@@ -3,10 +3,12 @@
  */
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    private var player: AVPlayer {AVPlayer.sharedDingPlayer}
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0)
@@ -21,6 +23,10 @@ struct MeetingView: View {
             .foregroundColor(scrum.theme.accentColor)
             .onAppear {
                 scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+                scrumTimer.speakerChangedAction = {
+                    player.seek(to: .zero)
+                    player.play()
+                }
                 scrumTimer.startScrum()
             }
             .onDisappear {
